a6b81b50e5f1a3097536e4a1045c22d87c62076b
acyushka <izotovarsenii3005@gmail.com>
2025-12-13 20:56:55 +0300

    Add: tests

---
+++ tests/test_casino.py
@@ -0,0 +1,128 @@
+import random
+from src.services.casino import Casino
+
+
+class TestCasino:
+    def test_register_player_random(self):
+        casino = Casino()
+        old_names_count = len(casino.names)
+
+        result = casino.register_player()
+
+        assert result is not None
+        assert "Игрок" in result
+        assert len(casino.players) == 1
+        assert len(casino.names) == old_names_count - 1
+
+    def test_register_player_custom(self):
+        casino = Casino()
+        result = casino.register_player("lala", 1488)
+
+        assert "lala" in result
+        assert casino.balances["lala"] == 1488
+        assert len(casino.players) == 1
+
+    def test_register_goose_war(self):
+        casino = Casino()
+        result = casino.register_goose("war", "lala", 5)
+
+        assert "Боевой гусь lala" in result
+        assert len(casino.gooses) == 1
+        assert casino.gooses_income["lala"] == 0
+
+    def test_register_goose_honk(self):
+        casino = Casino()
+        result = casino.register_goose("honk", "lala", 10)
+
+        assert "Гусь-крикун lala" in result
+        assert len(casino.gooses) == 1
+        assert casino.gooses_income["lala"] == 0
+
+    def test_dep_win(self, monkeypatch):
+        casino = Casino()
+        casino.register_player("lala", 100)
+
+        monkeypatch.setattr(random, 'random', lambda: 0.99)
+
+        result = casino.player_dep()
+
+        assert result is not None
+        assert "выиграл" in result
+        assert casino.steps_count == 1
+
+    def testdep_loss(self, monkeypatch):
+        casino = Casino()
+        casino.register_player("lala", 100)
+
+        monkeypatch.setattr(random, 'random', lambda: 0.89999)
+
+        result = casino.player_dep()
+
+        assert result is not None
+        assert "не залетела" in result
+
+    def test_dep_killed(self, monkeypatch):
+        casino = Casino()
+        casino.register_player("lala", 10)
+
+        monkeypatch.setattr(random, 'random', lambda: 0.5)
+        monkeypatch.setattr(random, 'randint', lambda a, b: 10)
+
+        result = casino.player_dep()
+
+        assert result is not None
+        assert "проиграл" in result
+        assert "Игрок" not in casino.balances
+        assert len(casino.players) == 0
+
+    def test_goose_action_1(self):
+        casino = Casino()
+        casino.register_player("lala", 10)
+        casino.register_goose("war", "osa", 10)
+
+        result = casino.goose_action()
+
+        assert result is not None
+        assert casino.steps_count == 1
+
+    def test_goose_action_no_gooses(self):
+        casino = Casino()
+        result = casino.goose_action()
+        assert result is None
+
+    def test_goose_group(self):
+        casino = Casino()
+        casino.register_player("wfe", 100)
+        casino.register_player("efw", 100)
+        casino.register_goose("war", "da", 10)
+        casino.register_goose("honk", "daads", 5)
+
+        result = casino.goose_group()
+
+        assert result is not None
+        assert "орава гусей" in result
+        assert casino.steps_count == 1
+
+    def test_panic(self):
+        casino = Casino()
+        casino.register_player("ladls", 100)
+        casino.register_goose("war", "dlsa", 10)
+
+        result = casino.panic_action()
+
+        assert result is not None
+        assert "запаниковал" in result
+        assert casino.steps_count == 1
+
+    def test_healing(self):
+        casino = Casino()
+        casino.register_player("rew", 100)
+
+        casino.players[0].hp -= 50
+
+        result = casino.healing_action()
+
+        assert result is not None
+        assert "энергетик" in result
+        assert casino.players[0].hp > 50
+        assert casino.steps_count == 1
---
+++ tests/test_collections.py
@@ -0,0 +1,96 @@
+import pytest
+from src.collections.player import PlayerCollection
+from src.models.player import Player
+from src.collections.goose import GooseCollection
+from src.models.gooses import Goose, WarGoose
+
+
+class TestPlayerCollection:
+    def test_append(self):
+        collection = PlayerCollection()
+        player = Player("houh", 1111)
+        collection.append(player)
+        assert len(collection) == 1
+        assert collection[0] == player
+
+    def test_append_invalid_type(self):
+        collection = PlayerCollection()
+        with pytest.raises(TypeError):
+            collection.append("ifks")
+
+    def test_remove(self):
+        collection = PlayerCollection()
+        player = Player("fsea;", 100)
+        collection.append(player)
+        collection.remove(player)
+        assert len(collection) == 0
+
+    def test_remove_invalid(self):
+        collection = PlayerCollection()
+        player = Player("Иван", 100)
+        with pytest.raises(Exception):
+            collection.remove(player)
+
+    def test_get_random_alive(self):
+        collection = PlayerCollection()
+        player1 = Player("ghdf", 10032)
+        player2 = Player("htrs", 100321)
+        player2.hp = 0
+
+        collection.append(player1)
+        collection.append(player2)
+
+        result = collection.get_random_alive_player()
+        assert result == player1
+
+    def test_iter(self):
+        collection = PlayerCollection()
+        player1 = Player("ghdf", 10032)
+        player2 = Player("htrs", 100321)
+        collection.append(player1)
+        collection.append(player2)
+
+        players = list(collection)
+        assert len(players) == 2
+        assert player1 in players
+        assert player2 in players
+
+
+class TestGooseCollection:
+    def test_append(self):
+        collection = GooseCollection()
+        goose = Goose("hfg", 5)
+        collection.append(goose)
+        assert len(collection) == 1
+        assert collection[0] == goose
+
+    def test_remove(self):
+        collection = GooseCollection()
+        goose = Goose("jkl", 5)
+        collection.append(goose)
+        collection.remove(goose)
+        assert len(collection) == 0
+
+    def test_get_random(self):
+        collection = GooseCollection()
+        goose1 = Goose("hrfts", 3)
+        goose2 = WarGoose("das", 10)
+        collection.append(goose1)
+        collection.append(goose2)
+
+        result = collection.get_random_goose()
+        assert result in [goose1, goose2]
+
+    def test_add(self):
+        collection1 = GooseCollection()
+        collection2 = GooseCollection()
+
+        goose1 = Goose("hrfts", 3)
+        goose2 = WarGoose("das", 10)
+        collection1.append(goose1)
+        collection2.append(goose2)
+
+        group = collection1 + collection2
+        assert len(group) == 2
+        assert goose1 in group.gooses
+        assert goose2 in group.gooses
